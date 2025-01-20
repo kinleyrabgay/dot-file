30b9ca1bf3ebfac784419d8ba1899555109d2c52
kinleyrabgay <fsd.rabgay@gmail.com>
2025-01-20 15:38:07 +0600

    first commit
    Signed-off-by: kinleyrabgay <fsd.rabgay@gmail.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,62 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Do thing without affecting the registers
+keymap.set("n", "x", '"_x')
+keymap.set("n", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>P", '"0P')
+keymap.set("v", "<Leader>p", '"0p')
+keymap.set("n", "<Leader>c", '"_c')
+keymap.set("n", "<Leader>C", '"_C')
+keymap.set("v", "<Leader>c", '"_c')
+keymap.set("v", "<Leader>C", '"_C')
+keymap.set("n", "<Leader>d", '"_d')
+keymap.set("n", "<Leader>D", '"_D')
+keymap.set("v", "<Leader>d", '"_d')
+keymap.set("v", "<Leader>D", '"_D')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Disable continuation
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- Jumplist
+keymap.set("n", "[%%", "<C-i>", opts)
+
+-- New tabs
+keymap.set("n", "te", "tabedit", opts)
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+
+-- Split windows
+keymap.set("n", "ss", ":split<Return>", opts)
+keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- Move windows
+keymap.set("n", "sh", "<C-w>h")
+keymap.set("n", "sk", "<C-w>k")
+keymap.set("n", "sj", "<C-w>j")
+keymap.set("n", "sl", "<C-w>l")
+
+-- Resize winndows
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,117 @@
+return {
+  { "sainnhe/edge" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      colorscheme = "edge",
+    },
+  },
+}
+
+-- return {
+--   { "shaunsingh/nord.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--       colorscheme = "nord",
+--     },
+--   },
+-- }
+
+-- return {
+--   { "folke/tokyonight.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--       colorscheme = "tokyonight",
+--     },
+--   },
+-- }
+
+-- return {
+--   { "joshdick/onedark.vim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--       colorscheme = "onedark",
+--     },
+--   },
+-- }
+
+-- return {
+--   "ellisonleao/gruvbox.nvim",
+--   priority = 1000,
+--   config = function()
+--     require("gruvbox").setup({})
+--     vim.cmd([[
+--       colorscheme gruvbox
+--       hi CocUnusedHighlight cterm=underline gui=underline guifg=#6B6860
+--     ]])
+--   end,
+-- }
+
+-- return {
+--   { "catppuccin/nvim", name = "catppuccin" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--       colorscheme = "catppuccin-mocha", -- Or latte, frappe, macchiato
+--     },
+--   },
+-- }
+
+-- return {
+--   { "sainnhe/everforest" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--       colorscheme = "everforest",
+--     },
+--   },
+-- }
+
+-- return {
+--   { "EdenEast/nightfox.nvim" },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--       colorscheme = "nightfox", -- Or nordfox, duskfox, etc.
+--     },
+--   },
+-- }
+
